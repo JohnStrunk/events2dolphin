cdfde571fd6fe6ab2e636940404a9cddc31fda29
John Strunk <john.strunk@gmail.com>
2020-08-16 13:55:26 -0400

    Add AGPL license text
    Signed-off-by: John Strunk <john.strunk@gmail.com>

--- events2dolphin.py
+++ events2dolphin.py
@@ -1,7 +1,24 @@
 """
-Converts a "CTS Start list" set of files from Meet Manager into an event list
-for the CTS Dolphin system.
+events2dolphin - Converts a "CTS Start list" set of files from Hy-Tek Meet
+Manager into an event list for the CTS Dolphin system.
 """
+
+# events2dolphin - https://github.com/JohnStrunk/events2dolphin
+# Copyright (C) 2020 - John D. Strunk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import copy
 import re
 import sys
